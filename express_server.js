fefb814f17435ed089f8428e02f5db396036c984
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-07 22:02:48 +0000

    add the post

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,9 +26,20 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  req.body.id = generateRandomString();
+  console.log(req.body.id);
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

9a858fc857819b71cc85714d4f63d6d3c76e52ea
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-07 23:35:44 +0000

    acess to a url from its id

--- express_server.js
+++ express_server.js
@@ -34,12 +34,18 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  req.body.id = generateRandomString();
-  console.log(req.body.id);
+app.post("/urls/", (req, res) => {
+  let shortURL = generateRandomString();
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(urlDatabase[shortURL]);
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
 
+});
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

8c0d86af581582a1014052597a1b112ca73bd091
Khaoula <khaoula.mouanniss@gmail.com>
2020-12-09 00:55:40 +0000

    Final

--- express_server.js
+++ express_server.js
@@ -25,31 +25,29 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+//showing all the urls
 app.get("/urls", (req, res) => {
  const templateVars = { username: req.cookies["name"], urls: urlDatabase };
  res.render("urls_index", templateVars);
- console.log(req.cookies);
 });
+//show the page for creating new url
 app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["name"]};
   res.render("urls_new",templateVars);
 });
+//show a specific url
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { username: req.cookies["name"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+//create new url
 app.post("/urls/", (req, res) => {
   let shortURL = generateRandomString();
-  console.log(req.body);  // Log the POST request body to the console
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(urlDatabase[shortURL]);
-  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-
+  res.redirect('/url');
 });
+//access to a url from its key
 app.get("/u/:shortURL", (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
   const shortURL = req.params.shortURL;
   if (!Object.prototype.hasOwnProperty.call(urlDatabase,shortURL)) {
     res.status(404);
@@ -59,24 +57,25 @@ app.get("/u/:shortURL", (req, res) => {
     res.redirect(longURL);
   }
 });
+//delete a url
 app.post("/urls/:shortURL/delete", (req, res) => {
-  //console.log(req.body);  // Log the POST request body to the console
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls/');
-
 });
+//update a url
 app.post("/urls/:shortURL/Update", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect('/urls/');
 });
+//login bu a username
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie('name', username);
-  //res.send(username);
   res.redirect('/urls');
 });
+//logout by clearing the cookies
 app.post("/logout", (req, res) => {
   res.clearCookie('name');
   res.redirect('/urls');
